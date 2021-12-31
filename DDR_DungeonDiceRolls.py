329cf1552525f44b2ca46bb7b154fbe05e6a7d74
Bracero <guibragon@gmail.com>
2021-12-31 12:55:42 -0300

    Up 0.1
    -Type Hinting added
    - booleans problem fixed
    - "if __name__ == "__main__": " method added

--- DDR_DungeonDiceRolls.py
+++ DDR_DungeonDiceRolls.py
@@ -1,37 +1,41 @@
 from random import randint
 from time import sleep
 
+
 # Class
 class Dice:
 
-    def __init__(self, faces, values):
+    def __init__(self, faces: int, values: int):
         self.faces = faces
         self.values = values
-    
+
     def check_faces(self):
         if self.faces != (4 or 6 or 8 or 10 or 12 or 20 or 100):
-            self.faces == False
-            if self.faces == False:
+            var = self.faces is False
+            if var:
                 print("Unavailable faces. Please, repeat the operation.")
                 return rolldice()
             else:
                 return
         else:
-            return 
+            return
 
-            
-# Dices sets 
+        # Dices sets
+
+
 ds1, ds2, ds3, ds4, ds5, ds6 = ([], [], [], [], [], [])
 
 # Roll
-temp = list()
+temp: list = list()
+fc: int = 0
+dc: int = 0
 
 
-def rolldice():
+def rolldice() -> int:
+    global fc, dc
     # part 1
-    print()
     try:
-        dc = int(input("Dice quantities:\n"))
+        dc = int(input("\nDice quantities:\n"))
         fc = int(input("Dice(s) faces:\n"))
     except ValueError as errb:
         print(f"Error in Roll section: {errb}")
@@ -47,14 +51,14 @@ def rolldice():
         print(f"{i}/{dc} roll: {rand}")
         i = i + 1
 
-
         # part 2
         def rolldicept2():
             print(f"\nresults:\n {temp}")
             decision = input("Want to storage this values? y/n\n")
             if decision == "y":
                 global ds1, ds2, ds3, ds4, ds5, ds6
-                dsopt = input(f"choose an empty variable to storage: \nds1: {ds1}\nds2: {ds2}\nds3: {ds3}\nds4: {ds4}\nds5: {ds5}\nds6: {ds6}\n\n")
+                dsopt = input(
+                    f"choose an empty variable to storage: \nds1: {ds1}\nds2: {ds2}\nds3: {ds3}\nds4: {ds4}\nds5: {ds5}\nds6: {ds6}\n\n")
                 if dsopt == "ds1":
                     ds1 = temp.copy()
                     return menu()
@@ -69,7 +73,7 @@ def rolldice():
                     return menu()
                 elif dsopt == "ds5":
                     ds5 = temp.copy()
-                    return menu()                   
+                    return menu()
                 elif dsopt == "ds6":
                     ds6 = temp.copy()
                     return menu()
@@ -77,19 +81,22 @@ def rolldice():
                     print("Only need to write 'ds+1, 2, 3, 5, 6'. \nTry again\n")
                     return rolldicept2()
             elif decision == "n":
-                temp.clear()                
+                temp.clear()
                 return menu()
             else:
                 return rolldicept2()
         # end of part 2
-
 
     return rolldicept2()
     # end of part 1
 
 
 # Menu
-def menu():
+opt: int = 0
+
+
+def menu() -> int:
+    global opt
     print('Dungeon Dice Rolls made with Python!\n(1) Roll dices\n(2) Dices storage\n(3) Quit aplication')
     try:
         opt = int(input('Tip an option below: '))
@@ -99,12 +106,13 @@ def menu():
     if opt == 1:
         rolldice()
     elif opt == 2:
-         print(f"\nds1: {ds1}\nds2: {ds2}\nds3: {ds3}\nds4: {ds4}\nds5: {ds5}\nds6: {ds6}\n\n")
-         sleep(3)
-         return menu()
+        print(f"\nds1: {ds1}\nds2: {ds2}\nds3: {ds3}\nds4: {ds4}\nds5: {ds5}\nds6: {ds6}\n\n")
+        sleep(3)
+        return menu()
     else:
         print("You need to tip a number between 1 - 3. \nTry again\n")
         menu()
 
 
-menu()
+if __name__ == "__main__":
+    menu()
